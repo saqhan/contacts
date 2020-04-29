8c4103e57bc44923192404cc13061dabb9554335
saqhan <saqhan1@mail.ru>
2020-04-29 03:53:07 +0300

    add switch style

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React from 'react';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 import "./header.css";
 
-const Header = ({switchStyle, darkTheme, userName }) => {  
+const Header = ({switchStyle, darkTheme, userName }) => {
+    const [state, setState] = React.useState({
+        checkedA: true,
+        checkedB: true,
+    });
+
+    const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
+
   return (
     <div className="chat-header">
       <div className="left-header">Hi, {userName}</div>
       <div className="right-header">
-        <button onClick={switchStyle}>
-          {darkTheme ? "дневной режим" : "ночной режим"}
-        </button>
+
+          <FormGroup row>
+              <FormControlLabel
+                  control={
+                      <Switch
+                          checked={state.checkedB}
+                          onChange={handleChange}
+                          name="checkedB"
+                          color="primary"
+                          onClick={switchStyle}
+                      />
+                  }
+                  label={darkTheme ? "дневной режим" : "ночной режим"}
+              />
+          </FormGroup>
       </div>
     </div>
   );
